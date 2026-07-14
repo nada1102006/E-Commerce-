4438529a2a3bebfc4f492e52a25762663e59a0d1
Osama Samy <149599819+Osama-Samy@users.noreply.github.com>
2026-07-14 20:59:22 +0300

    feat: project structure at root

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-slate-800">
+        SONDOS && AHMED && NESMA TASK
+      </h1>
+    </div>
+  );
+}
